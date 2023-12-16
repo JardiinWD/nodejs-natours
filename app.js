f4a837852315e8a6ed7b4c5f61eb6b0795a64e33
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-12-16 12:18:27 +0100

    Handling request to URL Id params

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const express = require('express');
 // Initializing the Express application
 const app = express();
-// Creating Middleware
+// Creating Middleware to parse JSON in requests
 app.use(express.json());
-
 
 //#region Trash
 
@@ -40,21 +39,49 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+// Handling GET requests to the '/api/v1/tours/:id' endpoint
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+
+    // Responding with a 404 status code for an invalid ID
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    // Responding with a 200 status code and the requested tour data
+    else {
+        return res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    }
+});
+
+// Handling POST requests to the '/api/v1/tours' endpoint
 app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body);
+    // Generating a new ID for the new tour
     const newId = tours[tours.length - 1].id + 1;
+    // Creating a new tour object by merging the new ID and request body
     const newTour = Object.assign({ id: newId }, req.body);
+    // Adding the new tour to the existing tours array
     tours.push(newTour);
+
+    // Writing the updated tour data back to the JSON file
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        // Responding with a 201 status code and the newly created tour data
         res.status(201).json({
             status: 'success',
             data: {
                 tour: newTour
             }
-        })
-    })
-})
-
+        });
+    });
+});
 
 // Specifying the port for the server to listen on
 const port = 7775;
