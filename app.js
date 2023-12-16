8e27cc411033f935163da580b10f1dcbbaec894f
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-12-16 11:41:55 +0100

    Created first POST request for new tour

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const express = require('express');
 // Initializing the Express application
 const app = express();
+// Creating Middleware
+app.use(express.json());
+
 
 //#region Trash
 
@@ -36,6 +39,22 @@ app.get('/api/v1/tours', (req, res) => {
         }
     });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
 
 // Specifying the port for the server to listen on
 const port = 7775;
