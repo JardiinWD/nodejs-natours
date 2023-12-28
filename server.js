595d875b5dca136c022f359eed7fe624860a023f
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-12-28 17:52:07 +0100

    Created eslint rules, installed new packages and connected hosted DB with mongoose

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
+// Importing the mongoose package for connecting MongoDb
+const mongoose = require('mongoose')
 // Importing the dotenv package for environment variable configuration
 const dotenv = require('dotenv');
+// Importing the Express app from app.js
+const app = require('./app');
+
 // Configuring dotenv and specifying the path for the environment variables file
 dotenv.config({
     path: './config.env'
 });
 
-// Importing the Express app from app.js
-const app = require('./app');
+// Setting the MongoDB Connection strings with .env variables
+const mongoDbUri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_PROJECT}/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+
+
+// METHOD 1 -> Connect to Hosted Atlas Database
+
+mongoose.connect(mongoDbUri, {
+    useNewUrlParser: true,       // Use the new URL parser
+    useCreateIndex: true,        // Ensure indexes are created when needed
+    useFindAndModify: false,     // Disable 'findOneAndUpdate' and 'findOneAndDelete' to use 'updateOne' and 'deleteOne'
+    useUnifiedTopology: true     // Use the new Server Discovery and Monitoring engine
+}).then(() => {
+    console.log("DB Connection successful");
+})
+
+// METHOD 2 -> Connect to Local Database
+
+/* mongoose.connect(process.env.MONGO_DB_LOCAL , {
+    useNewUrlParser: true,       // Use the new URL parser
+    useCreateIndex: true,        // Ensure indexes are created when needed
+    useFindAndModify: false,     // Disable 'findOneAndUpdate' and 'findOneAndDelete' to use 'updateOne' and 'deleteOne'
+    useUnifiedTopology: true     // Use the new Server Discovery and Monitoring engine
+}).then(() => {
+    console.log("DB Connection successful");
+}) */
+
 
 // Specifying the port for the server to listen on
 const port = process.env.PORT || 3375;
